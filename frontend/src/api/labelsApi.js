16b0b6447eeacd5d7b521071a87facafa7c7b365
Talisson Cleofas <108986319+Talisson-Cleofas@users.noreply.github.com>
2025-06-16 10:40:37 -0300

    Update labelsApi.js

--- frontend/src/api/labelsApi.js
+++ frontend/src/api/labelsApi.js
@@ -1,5 +1,5 @@
 // src/api/labelsApi.js
-const API_BASE_URL = "http://localhost:3000"; // ou sua URL real do backend
+const API_BASE_URL = "https://marshall-whatsapp-auto.onrender.com"; // ou sua URL real do backend
 
 export async function getLabels() {
   const response = await fetch(`${API_BASE_URL}/labels`);
